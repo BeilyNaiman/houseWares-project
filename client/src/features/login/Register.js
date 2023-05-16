ce4d531ccf40453f02de451a06b1f473018a0426
BeilyNaiman <beily06407@gmail.com>
2023-05-16 16:26:56 +0300

    first

---
+++ client/src/features/login/Register.js
@@ -0,0 +1,53 @@
+import './Login.css';
+import React, { useState,useRef } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { addNewUserToServer, login } from '../../users/UserSlice';
+import { useDispatch } from 'react-redux';
+import {  useNavigate } from 'react-router';
+
+export default function Login() {
+ let disp=useDispatch();
+ let navigate=useNavigate();
+ const inputName=useRef(null);
+ const inputPass=useRef(null);
+ const inputPhon=useRef(null);
+ const inputMail=useRef(null);
+
+const sentToUserSlice=()=>{
+    disp(addNewUserToServer({name:inputName.current.value,pass:inputPass.current.value,phon:inputPhon.current.value,email:inputMail.current.value}))
+    navigate('/Products')
+}
+  return (
+  <>
+    <div className="Login">  
+      <Form className="form" >
+         <div className="Auth-form-content">
+         <Form.Group size="lg" controlId="userName" >       
+           <Form.Label className='label'>Name</Form.Label>
+           <Form.Control  placeholder="Enter your name" ref={inputName} autoFocus  type="name"  
+        />
+         </Form.Group>
+        <Form.Group size="lg" controlId="password" >
+           <Form.Label className='label'>Password</Form.Label>
+           <Form.Control placeholder=" Enter your password" ref={inputPass}  type="password"  
+         />
+         </Form.Group>
+        <Form.Group size="lg" controlId="userPhon" >       
+           <Form.Label className='label'>Phon</Form.Label>
+           <Form.Control  placeholder="Enter your phon" ref={inputPhon} autoFocus  type="phon"
+        />
+        </Form.Group>
+          <Form.Group size="lg" controlId="userMail" >       
+           <Form.Label className='label'>Mail</Form.Label>
+           <Form.Control  placeholder="Enter your mail" ref={inputMail} autoFocus  type="email" 
+        />
+          </Form.Group>
+            </div>
+            <br/>
+          <Button width="20" onClick={()=>{sentToUserSlice()}}>Register</Button>
+      </Form>
+    </div>
+  </>
+  );
+}
